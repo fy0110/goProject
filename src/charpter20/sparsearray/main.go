85d83d3e585db8e3ff52d77edab489ce3fc213c9
fanyang <1070108716@qq.com>
2020-09-10 10:49:51 +0800

    Default Changelist

---
+++ src/charpter20/sparsearray/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type ValNode struct {
+	row int
+	col int
+	val int
+}
+
+func main() {
+
+	var chessMap [11][11]int
+	chessMap[1][2] = 1
+	chessMap[2][3] = 2
+	for _, v := range chessMap {
+		for _, v2 := range v {
+			fmt.Print(" ", v2)
+		}
+		fmt.Println()
+	}
+	var sparseArr []ValNode
+	valNode := ValNode{
+		row: 11,
+		col: 11,
+		val: 0,
+	}
+	sparseArr = append(sparseArr, valNode)
+	for i, i2 := range chessMap {
+		for i3, i4 := range i2 {
+			if i4 != 0 {
+				valNode := ValNode{
+					row: i,
+					col: i3,
+					val: i4,
+				}
+				sparseArr = append(sparseArr, valNode)
+			}
+		}
+	}
+	fmt.Println(sparseArr)
+}
