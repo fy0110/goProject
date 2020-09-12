562ec5010a0f6a921b5b3d83eac8fb7d0fa9bc2b
fanyang <1070108716@qq.com>
2020-09-12 21:06:21 +0800

    Default Changelist

--- src/charpter20/sort/selectsort/main.go
+++ src/charpter20/sort/selectsort/main.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
-func selectsort(arr *[5]int) {
+//选择当前第一个为最大值进行比较
+func selectsort(arr *[6]int) {
 	for j := 0; j < len(arr)-1; j++ {
 		maxnum := arr[j]
 		maxIndex := j
-		for i := j; i < len(arr); i++ {
+		for i := j; i < len(arr)-j; i++ {
 			if arr[i] > maxnum {
 				maxnum = arr[i]
 				maxIndex = i
@@ -19,7 +20,7 @@ func selectsort(arr *[5]int) {
 }
 
 func main() {
-	arr := [5]int{10, 34, 19, 100, 80}
+	arr := [6]int{10, 34, 19, 100, 80, 6}
 	selectsort(&arr)
 	fmt.Println(arr)
 }
