95bffc8e26084731836603cb8aed9da5b774f701
fanyang <1070108716@qq.com>
2020-09-15 10:54:55 +0800

    Default Changelist

---
+++ src/charpter20/UserStack/main.go
@@ -0,0 +1,22 @@
+package main
+
+type numstack struct {
+	maxTop int
+	Top    int
+	numarr [10]int
+}
+
+type sigelstack struct {
+	maxTop   int
+	Top      int
+	sigelarr [10]int
+}
+
+func PushStack(Arithmetic string) (err error) {
+
+	return nil
+}
+
+func main() {
+
+}
