80b9eda9b3e96a4d10a4d263fa3fbf7f0c377d65
fanyang <1070108716@qq.com>
2020-09-15 14:05:51 +0800

    Default Changelist

--- src/charpter20/UserStack/main.go
+++ src/charpter20/UserStack/main.go
@@ -1,22 +1,68 @@
 package main
 
-type numstack struct {
+import (
+	"errors"
+	"fmt"
+)
+
+type Stack struct {
 	maxTop int
 	Top    int
-	numarr [10]int
+	arr    [20]int
 }
 
-type sigelstack struct {
-	maxTop   int
-	Top      int
-	sigelarr [10]int
+//判断是不是操作符
+func (this *Stack) isOper(val int) bool {
+	if val == 42 || val == 43 || val == 45 || val == 47 {
+		return true
+	} else {
+		return false
+	}
 }
 
-func PushStack(Arithmetic string) (err error) {
+func (this *Stack) Push(val int) (err error) {
+	if this.Top == this.maxTop-1 {
+		return errors.New("栈已满")
+	}
+	this.Top++
+	this.arr[this.Top] = val
+	return
+}
 
-	return nil
+func (this *Stack) List() (err error) {
+	if this.Top == -1 {
+		return errors.New("空栈")
+	}
+	for i := this.Top; i >= 0; i-- {
+		fmt.Println(this.arr[i])
+	}
+	return
+}
+
+func (this *Stack) Pop() (val int, err error) {
+	if this.Top == -1 {
+		return 0, errors.New("空栈")
+	}
+	val = this.arr[this.Top]
+	this.Top--
+	return val, nil
 }
 
 func main() {
-
+	//numstack := &Stack{
+	//	maxTop: 20,
+	//	Top:    -1,
+	//	arr:    [20]int{},
+	//}
+	//operstack := &Stack{
+	//	maxTop: 20,
+	//	Top:    -1,
+	//	arr:    [20]int{},
+	//}
+	//exp:="3+2*6-2"
+	//index := 0
+	//for  {
+	//	ch:=exp[index:index+1]
+	//
+	//}
 }
